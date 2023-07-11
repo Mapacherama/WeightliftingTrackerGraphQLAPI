f692c37b51b9d0ab58eb31c8c3584cfb1b391908
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-07-11 12:30:20 +0200

    Feature: Wrote test to see if MySQL configuration is succesfull

---
+++ WeightliftingTrackerGraphQLAPI.Tests/MySqlDataAccessTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Data;
+using WeightliftingTrackerGraphQLAPI.Data;
+
+namespace WeightliftingTrackerGraphQLAPI.Tests
+{
+    [TestFixture]
+    public class MySqlDataAccessTests
+    {
+        private const string ConnectionString = "SERVER=localhost;DATABASE=weightlifting_tracker;UID=root;PASSWORD=admin;";
+
+        [Test]
+        public void TestExecuteQuery()
+        {
+            string query = "SELECT * FROM Workout";
+            MySqlDataAccess dataAccess = new MySqlDataAccess(ConnectionString);
+
+            try
+            {
+                DataTable result = dataAccess.ExecuteQuery(query);
+
+                // Display the retrieved data
+                Console.WriteLine("Workout Data:");
+                foreach (DataRow row in result.Rows)
+                {
+                    Console.WriteLine($"Id: {row["Id"]}, ExerciseName: {row["ExerciseName"]}, Sets: {row["Sets"]}, Reps: {row["Reps"]}, Weight: {row["Weight"]}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing query: {ex.Message}");
+            }
+        }
+    }
+}
