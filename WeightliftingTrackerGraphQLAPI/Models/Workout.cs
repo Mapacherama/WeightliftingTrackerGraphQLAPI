0dd7aaf06440ba37493c5eac28aa537ceef3adaa
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-07-11 09:06:31 +0200

    Feature: Started implementing the workout object

---
+++ WeightliftingTrackerGraphQLAPI/Models/Workout.cs
@@ -0,0 +1,12 @@
+﻿namespace WeightliftingTrackerGraphQLAPI.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+        public string? ExerciseName { get; set; }
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+        public decimal Weight { get; set; }
+        // Add additional properties as needed
+    }
+}
---
+++ WeightliftingTrackerGraphQLAPI/Resolvers/WorkoutResolvers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WeightliftingTrackerGraphQLAPI.Models;
+
+namespace WeightliftingTrackerGraphQLAPI.Resolvers
+{
+    public class WorkoutResolvers
+    {
+        private readonly List<Workout> _workouts; // Assume this is your data source
+
+        public WorkoutResolvers()
+        {
+            _workouts = new List<Workout>(); // Initialize or fetch the workouts from your data source
+        }
+
+        public IEnumerable<Workout> GetWorkouts()
+        {
+            return _workouts;
+        }
+    }
+}
