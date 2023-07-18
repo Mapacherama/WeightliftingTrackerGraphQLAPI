c5c47e1acc56c395bad9520b295d197a6b16363a
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-07-18 10:15:50 +0200

    Feature: Added data row helper

--- WeightliftingTrackerGraphQLAPI/Repositories/WorkoutRepository.cs
+++ WeightliftingTrackerGraphQLAPI/Repositories/WorkoutRepository.cs
@@ -2,12 +2,16 @@
 using System.Data;
 using WeightliftingTrackerGraphQLAPI.Data;
 using WeightliftingTrackerGraphQLAPI.Models;
+using WeightliftingTrackerGraphQLAPI.Helpers;
 
 namespace WeightliftingTrackerGraphQLAPI.Repositories
 {
     public class WorkoutRepository : IWorkoutRepository
     {
         private readonly IMySqlDataAccess _dataAccess;
+        private const string SelectQuery = "SELECT * FROM Workout WHERE ExerciseName = @ExerciseName AND Sets = @Sets AND Reps = @Reps AND Weight = @Weight;";
+        private const string InsertQuery = "INSERT INTO Workout (ExerciseName, Sets, Reps, Weight) VALUES (@ExerciseName, @Sets, @Reps, @Weight);";
+        private const string SelectLastId = "SELECT LAST_INSERT_ID();";
 
         public WorkoutRepository(IMySqlDataAccess dataAccess)
         {
@@ -21,28 +25,47 @@ namespace WeightliftingTrackerGraphQLAPI.Repositories
                 throw new ArgumentNullException(nameof(newWorkout));
             }
 
-            MySqlParameter[] parameters = new MySqlParameter[]
-            {
-                new MySqlParameter("@ExerciseName", newWorkout.ExerciseName),
-                new MySqlParameter("@Sets", newWorkout.Sets),
-                new MySqlParameter("@Reps", newWorkout.Reps),
-                new MySqlParameter("@Weight", newWorkout.Weight)
-            };
-
-            string selectQuery = "SELECT * FROM Workout WHERE ExerciseName = @ExerciseName AND Sets = @Sets AND Reps = @Reps AND Weight = @Weight;";
-            DataTable dt = _dataAccess.ExecuteQuery(selectQuery, parameters);
+            DataTable dt = _dataAccess.ExecuteQuery(SelectQuery, CreateParameters(newWorkout));
 
             if (dt != null && dt.Rows.Count > 0)
             {
-                throw new Exception("A workout with the same details already exists.");
+                throw new Exception(ErrorMessages.WorkoutDetailsExists);
             }
 
-            string insertQuery = "INSERT INTO Workout (ExerciseName, Sets, Reps, Weight) VALUES (@ExerciseName, @Sets, @Reps, @Weight);";
-            _dataAccess.ExecuteQuery(insertQuery, parameters);
+            
+            _dataAccess.ExecuteQuery(InsertQuery, CreateParameters(newWorkout));
 
-            newWorkout.Id = Convert.ToInt32(_dataAccess.ExecuteScalar("SELECT LAST_INSERT_ID();", null));
+            newWorkout.Id = Convert.ToInt32(_dataAccess.ExecuteScalar(SelectLastId, null));
 
             return newWorkout;
+        }
+
+        private MySqlParameter[] CreateParameters(Workout workout)
+        {
+            return new MySqlParameter[]
+            {
+        new MySqlParameter("@ExerciseName", workout.ExerciseName),
+        new MySqlParameter("@Sets", workout.Sets),
+        new MySqlParameter("@Reps", workout.Reps),
+        new MySqlParameter("@Weight", workout.Weight)
+            };
+        }
+
+        private Workout WorkoutFromDataRow(DataRow row)
+        {
+            if (row.IsNull("Id") || row.IsNull("ExerciseName") || row.IsNull("Sets") || row.IsNull("Reps") || row.IsNull("Weight"))
+            {
+                throw new NullReferenceException(ErrorMessages.RowValueIsNull);
+            }
+
+            return new Workout
+            {
+                Id = Convert.ToInt32(row["Id"]),
+                ExerciseName = row["ExerciseName"].ToString(),
+                Sets = Convert.ToInt32(row["Sets"]),
+                Reps = Convert.ToInt32(row["Reps"]),
+                Weight = Convert.ToDecimal(row["Weight"])
+            };
         }
 
         public Workout DeleteWorkout(int workoutId)
@@ -53,41 +76,15 @@ namespace WeightliftingTrackerGraphQLAPI.Repositories
         public IEnumerable<Workout> GetWorkouts()
         {
             string query = "SELECT * FROM Workout";
-            MySqlParameter[] parameters = null; // You can add parameters if needed.
 
-            if (_dataAccess == null)
-            {
-                throw new NullReferenceException("_dataAccess is null");
-            }
-
-            DataTable dt = _dataAccess.ExecuteQuery(query, parameters);
+            DataTable dt = _dataAccess.ExecuteQuery(query, null);
 
             if (dt == null)
             {
-                throw new NullReferenceException("dt is null");
+                throw new NullReferenceException(ErrorMessages.DataTableIsNull);
             }
 
-            List<Workout> workouts = new List<Workout>();
-
-            foreach (DataRow row in dt.Rows)
-            {
-                if (row.IsNull("Id") || row.IsNull("ExerciseName") || row.IsNull("Sets") || row.IsNull("Reps") || row.IsNull("Weight"))
-                {
-                    throw new NullReferenceException("One of the row values is null");
-                }
-
-                Workout workout = new Workout
-                {
-                    Id = Convert.ToInt32(row["Id"]),
-                    ExerciseName = row["ExerciseName"].ToString(),
-                    Sets = Convert.ToInt32(row["Sets"]),
-                    Reps = Convert.ToInt32(row["Reps"]),
-                    Weight = Convert.ToDecimal(row["Weight"])
-                };
-                workouts.Add(workout);
-            }
-
-            return workouts;
+            return dt.AsEnumerable().Select(row => WorkoutFromDataRow(row)).ToList();
         }
 
         public Workout UpdateWorkout(Workout updatedWorkout)
