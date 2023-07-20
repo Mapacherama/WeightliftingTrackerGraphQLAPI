08ae79c7590f3dc0f80578f659a19f3831d5213d
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-07-20 12:30:04 +0200

    Feature: Start setting up authentication

--- WeightliftingTrackerGraphQLAPI/Configuration/ApplicationConfigurations.cs
+++ WeightliftingTrackerGraphQLAPI/Configuration/ApplicationConfigurations.cs
@@ -12,8 +12,28 @@
             services.AddCors(options =>
             {
                 options.AddPolicy("Open",
-                    builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+                    builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials());
             });
+        }
+
+        public static void ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings").Get<JwtSettings>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtSettings.Issuer,
+                        ValidAudience = jwtSettings.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+                    };
+                });
         }
     }
 }
