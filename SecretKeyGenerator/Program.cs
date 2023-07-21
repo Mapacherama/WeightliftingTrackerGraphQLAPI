e3fc70219e3478d225d0cf51c35bd0fb900e7ee2
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-07-21 09:12:26 +0200

    Feature: SecretKeyGenerator implemented inside the project

---
+++ SecretKeyGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using Helpers;
+
+namespace SecretKeyGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string secretKey = TemporarySecretKeyGenerator.GenerateTemporarySecretKey();
+            Console.WriteLine(secretKey);
+        }
+    }
+}
