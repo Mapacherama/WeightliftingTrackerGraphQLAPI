44090ea5c8c7864ad0308efdec1d3e4bf7c9c293
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-07-21 10:12:21 +0200

    Feature: Simple login functionality

---
+++ WeightliftingTrackerGraphQLAPI/Resolvers/UserResolvers.cs
@@ -0,0 +1,28 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using WeightliftingTrackerGraphQLAPI.Models;
+
+namespace WeightliftingTrackerGraphQLAPI.Resolvers
+{
+    public class UserResolvers
+    {
+        public string Login(User user)
+        {
+            
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("<Your JWT Secret Key>");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return tokenString;
+        }
+    }
+}
