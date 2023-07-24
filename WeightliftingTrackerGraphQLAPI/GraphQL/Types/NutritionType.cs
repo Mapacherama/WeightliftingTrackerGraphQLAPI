aebbc2e67370168ad01663edb9cdec0a050c73e5
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-07-24 13:31:29 +0200

    Chore: Trying to fix a bug

--- WeightliftingTrackerGraphQLAPI/GraphQL/Types/NutritionType.cs
+++ WeightliftingTrackerGraphQLAPI/GraphQL/Types/NutritionType.cs
@@ -1,16 +1,15 @@
-﻿using WeightliftingTrackerGraphQLAPI.Models;
+﻿using HotChocolate.Types;
+using WeightliftingTrackerGraphQLAPI.Models;
 
-namespace WeightliftingTrackerGraphQLAPI.GraphQL.Types
+public class NutritionType : ObjectType<Nutrition>
 {
-    public class NutritionType : ObjectType<Nutrition>
+    protected override void Configure(IObjectTypeDescriptor<Nutrition> descriptor)
     {
-        protected override void Configure(IObjectTypeDescriptor<Nutrition> descriptor)
-        {
-            descriptor.Field(n => n.FoodName).Type<NonNullType<StringType>>();
-            descriptor.Field(n => n.Calories).Type<NonNullType<FloatType>>();
-            descriptor.Field(n => n.Protein).Type<NonNullType<FloatType>>();
-            descriptor.Field(n => n.Carbohydrates).Type<NonNullType<FloatType>>();
-            descriptor.Field(n => n.Fats).Type<NonNullType<FloatType>>();
-        }
+        descriptor.Field(x => x.Id).Type<IdType>().Description("The unique identifier of the nutrition.");
+        descriptor.Field(x => x.FoodName).Type<StringType>().Description("The name of the food.");
+        descriptor.Field(x => x.Calories).Description("The number of calories in the food.");
+        descriptor.Field(x => x.Protein).Description("The amount of protein in the food.");
+        descriptor.Field(x => x.Carbohydrates).Description("The amount of carbohydrates in the food.");
+        descriptor.Field(x => x.Fats).Description("The amount of fats in the food.");
     }
 }
